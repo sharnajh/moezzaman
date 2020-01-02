d99c05ed61eb0d0e4fe45a6ed1dd15a64fb68ede
Sharna Hossain <sharnajhossain@gmail.com>
2020-01-02 11:55:16 -0500

    implemented email api

--- src/Message.js
+++ src/Message.js
@@ -1,8 +1,54 @@
 import React, { Component } from "react";
-import MessageForm from "./MessageForm";
 import anime from "animejs/lib/anime.es.js";
 
 class Message extends Component {
+  state = {
+    name: "",
+    email: "",
+    message: ""
+  };
+
+  setName = e => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+  };
+
+  setEmail = e => {
+    e.preventDefault();
+    const text = e.target.value.trim();
+    this.setState({ email: text });
+  };
+
+  setMessage = e => {
+    e.preventDefault();
+    this.setState({ message: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const template_id = "moe_zzaman";
+    this.sendFeedback(template_id, {
+      message: this.state.message,
+      user_name: this.state.name,
+      user_email: this.state.email
+    });
+  };
+
+  sendFeedback(template_id, variables) {
+    window.emailjs
+      .send("gmail", template_id, variables)
+      .then(res => {
+        console.log("Email successfully sent!");
+      })
+      // Handle errors here however you like, or use a React error boundary
+      .catch(err =>
+        console.error(
+          "Oh well, you failed. Here some thoughts on the error that occured:",
+          err
+        )
+      );
+  }
+
   componentDidMount() {
     anime(
       {
@@ -12,53 +58,53 @@ class Message extends Component {
         easing: "easeOutExpo"
       },
       0
-    )
-    anime({
-      targets: ".section",
-      opacity: [0,1],
-      duration: 5000
-    }, 1500)
+    );
+    anime(
+      {
+        targets: ".section",
+        opacity: [0, 1],
+        duration: 5000
+      },
+      1500
+    );
   }
+
   render() {
+    console.log(this.state.name, this.state.email, this.state.message);
     return (
       <div className="section" style={{ alignItems: "space-between" }}>
-        <h3
-          style={{
-            zIndex: "1",
-            display: "block",
-            color: "#fff",
-            margin: "0",
-            marginTop: "50px",
-            padding: "0",
-            textAlign: "center"
-          }}
-        >
-          Contact Moe at
-          <br />
-          moezzaman3@gmail.com
-        </h3>
-        <MessageForm />
-        <h3
-          style={{
-            zIndex: 90,
-            color: "#fff",
-            margin: "0",
-            marginBottom: "50px",
-            padding: "0",
-            textAlign: "center"
-          }}
-        >
-          or connect with him
-          <br /> on{" "}
-          <a
-            href="https://www.linkedin.com/in/mohammed-asaduzzaman/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-          .
-        </h3>
+        <h3 id="mail">Contact Moe</h3>
+        <form className="message-form" onSubmit={this.handleSubmit}>
+          <input
+            className="message-input"
+            id="name"
+            type="text"
+            placeholder="Type your name here"
+            value={this.state.name}
+            onChange={this.setName}
+          ></input>
+
+          <input
+            className="message-input"
+            id="email"
+            type="email"
+            placeholder="Type your email here"
+            value={this.state.email}
+            onChange={this.setEmail}
+          ></input>
+
+          <textarea
+            className="message-input"
+            id="message"
+            rows="5"
+            placeholder="Type your message here"
+            value={this.state.message}
+            onChange={this.setMessage}
+            
+          ></textarea>
+
+          <button type="submit">Send Message</button>
+        </form>
       </div>
     );
   }
