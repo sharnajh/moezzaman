6372bb8f1941ffc72e590088f70e8b9ec28a317d
Sharna Hossain <sharnajhossain@gmail.com>
2020-01-29 03:52:43 -0500

    i know it was you

--- src/Message.js
+++ src/Message.js
@@ -100,7 +100,7 @@ class Message extends Component {
             </div>
           </div>
         )}
-        <div className="section" id="msg-section">
+        <div className="section" id="msg-section" style={{ justifyContent: "flex-start"}}>
           <div className="header">
             <div className="picwrapper">
               <div className="contactpic" />
